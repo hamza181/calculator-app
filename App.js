6fc635e0e131334a83e97c986d9bca65857a8450
Ameer Hamza <ameer.hamza1811@gmail.com>
2021-09-26 16:41:00 +0500

    insert buttons

--- App.js
+++ App.js
@@ -1,21 +1,83 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  NativeModules,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import Constants from "expo-constants";
+import MyButton from "./components/MyButton";
 
 export default function App() {
+
+  const calcValues = [
+    ["%", "CE", "C", "DEL"],
+    ["1/x", "x2", "2/x", "/"],
+    ["7", "8", "9", "X"],
+    ["4", "5", "6", "-"],
+    ["1", "2", "3", "+"],
+    ["+/-", "0", ".", "="],
+  ];
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-    </View>
+      <View style={styles.header}>
+        <Text style={styles.heading}>Calculator</Text>
+      </View>
+      <View style={styles.screen}></View>
+      <View style={styles.memoryRow}></View>
+      <View style={styles.buttonView}>
+        {calcValues.map((val, ind) => {
+          return (
+            <View style={styles.buttonRow}>
+              {val.map((value, index) => {
+                return (
+                  <View style={styles.mybutton}>
+                    <MyButton>{value}</MyButton>
+                  </View>
+                );
+              })}
+            </View>
+          );
+        })}
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ececec",
+    marginTop: Constants.statusBarHeight,
+    padding: 10,
+  },
+  header: {flex: 1},
+  heading: {
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+  screen: {flex: 3},
+  memoryRow: {flex: 1},
+  buttonView: {
+    paddingVertical: 10,
+    flex: 5
+    // backgroundColor: "red",
+  },
+  buttonRow: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    // backgroundColor: "green",
+    width: "100%",
+    paddingVertical: 2
+  },
+  mybutton: {
+    paddingHorizontal: 2,
+    width: "25%",
+    height: 50,
+    // backgroundColor: 'red'
   },
 });
---
+++ components/MyButton.js
@@ -0,0 +1,28 @@
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+
+export default function MyButton(props) {
+  return (
+    <TouchableOpacity onPress={props.onPress}>
+      <View style={styles.background}>
+        <Text style={styles.buttonText}>{props.children}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: "rgb(243, 243, 243)",
+    // backgroundColor: "yellow",
+    borderColor: "black",
+    padding: 10,
+    height: 50,
+    textAlign: "center",
+    justifyContent: "center",
+    alignItems: 'center'
+  },
+  buttonText: {
+    color: "rgb(68, 68, 68)",
+  },
+});
